4971ff1baac82bc96d963e9d5c62487c1199e732
Exvmon <exvmon@hotmail.com>
2012-02-16 18:30:59 +0100

    Signed-off-by: Exvmon <exvmon@hotmail.com>

---
+++ Icc.sql
@@ -0,0 +1,99 @@
+UPDATE `creature_text` SET text = '¿Así que por fin ha llegado la elogiada justicia de la Luz? ¿Debería deponer la Agonía de Escarcha y confiar en tu piedad, Vadín?' WHERE text LIKE '%So%the%Light\'s%vaunted%justice%has%finally%arrived?%Shall%I%lay%down%Frostmourne%and%throw%myself%at%your%mercy,%Fordring?%';
+
+UPDATE `creature_text` SET text = 'Te daremos una muerte rápida, Arthas. Más de lo que tuvieron aquellos a los que torturaste y asesinaste.' WHERE text LIKE '%We\'ll%grant%you%a%swift%death,%Arthas.%More%than%can%be%said%for%the%thousands%you\'ve%tortured%and%slain.%';
+
+UPDATE `creature_text` SET text = 'Lo sabrás de primera mano. Cuando haya acabado mi obra, suplicarás clemencia... y te la negaré. Tus afligidos gritos serán el testimonio de mi ilimitado poder...' WHERE text LIKE '%You\'ll%learn%of%that%first%hand.%When%my%work%is%complete,%you%will%beg%for%mercy%--%and%I%will%deny%you.%Your%anguished%cries%will%be%testament%to%my%unbridled%power...%';
+
+UPDATE `creature_text` SET text = 'Que así sea. Campeones, ¡ATACAD!' WHERE text LIKE '%So%be%it.%Champions,%attack!%';
+
+UPDATE `creature_text` SET text = '¡QUE LA LUZ ME BENDIGA POR ÚLTIMA VEZ! DAME FUERZAS... ¡PARA ROMPER ESTAS ATADURAS!' WHERE text LIKE '%LIGHT,%GRANT%ME%ONE%FINAL%BLESSING.%GIVE%ME%THE%STRENGTH...%TO%SHATTER%THESE%BONDS!%';
+
+UPDATE `creature_text` SET text = '¡Basta, Arthas! ¡¡¡Tu odio ya no consumirá más vidas!!!' WHERE text LIKE '%No%more,%Arthas!%No%more%lives%will%be%consumed%by%your%hatred!%';
+
+UPDATE `creature_text` SET text = 'Lo sabrás de primera mano. Cuando haya acabado mi obra, suplicarás clemencia... y te la negaré. Tus afligidos gritos serán el testimonio de mi ilimitado poder...' WHERE text LIKE '%I\'ll%keep%you%alive%to%witness%the%end,%Fordring.%I%would%not%want%the%Light\'s%greatest%champion%to%miss%seeing%this%wretched%world%remade%in%my%image.%';
+
+UPDATE `creature_text` SET text = 'Adiós esperanza...' WHERE text LIKE '%Hope%wanes!%';
+
+UPDATE `creature_text` SET text = '¡El fin ha llegado!' WHERE text LIKE '%The%end%has%come!%';
+
+UPDATE `creature_text` SET text = 'Os congelaré las entrañas hasta convertiros en un caparazón de hielo.' WHERE text LIKE '%I%will%freeze%you%from%within%until%all%that%remains%is%an%icy%husk!%';
+
+UPDATE `creature_text` SET text = '¡Contemplad el ocaso de vuestro mundo!' WHERE text LIKE '%Watch%as%the%world%around%you%collapses!%';
+
+UPDATE `creature_text` SET text = 'Val\'kir, vuestro maestro os llama.' WHERE text LIKE '%Val\'kyr,%your%master%calls!%';
+
+UPDATE `creature_text` SET text = 'La Agonía de Escarcha está hambrienta.' WHERE text LIKE '%Frostmourne%hungers...%';
+
+UPDATE `creature_text` SET text = 'No quedan preguntas sin respuesta. No quedan dudas. SOIS los mejores campeones de Azeroth. Superasteis todos los retos que os puse. Mis más poderosos sirvientes han caído ante vuestra implacable ofensiva... vuestra furia desatada...' WHERE text LIKE '%No%questions%remain%unanswered.%No%doubts%linger.%You%ARE%Azeroth\'s%greatest%champions.%You%overcame%every%challenge%I%laid%before%you.%My%mightiest%servants%have%fallen%before%your%relentless%onslaught...%your%unbridled%fury...%';
+
+UPDATE `creature_text` SET text = '¿Me pregunto si de verdad os mueve la... rectitud?' WHERE text LIKE '%Is%it%truly%the%righteousness%that%drives%you?%I%wonder...%';
+
+UPDATE `creature_text` SET text = 'Los has entrenado bien, Vadín. Has traído a los mejores luchadores que este mundo ha conocido... ante mí, ¡justo como yo quería! Te recompensaré por tu sacrificio involuntario.' WHERE text LIKE '%You%trained%them%well,%Fordring.%You%delivered%the%greatest%fighting%force%this%world%has%ever%known...%right%into%my%hands%--%exactly%as%I%intended.%You%shall%be%rewarded%for%your%unwitting%sacrifice.%';
+
+UPDATE `creature_text` SET text = 'Observa cómo los alzo para que se conviertan en maestros de la Plaga. Sumirán el mundo en caos y destrucción. La caída de Azeroth llegará de sus manos... Y tú serás el primero en morir.' WHERE text LIKE '%Watch%now%as%I%raise%them%from%the%dead%to%become%masters%of%the%Scourge.%They%will%shroud%this%world%in%chaos%and%destruction.%Azeroth\'s%fall%will%come%at%their%hands%--%and%you%will%be%the%first%to%die.%';
+
+UPDATE `creature_text` SET text = 'Me encanta esta ironía.' WHERE text LIKE '%I%delight%in%the%irony...%';
+
+UPDATE `creature_text` SET text = 'Imposible...' WHERE text LIKE '%Impossible...%';
+
+UPDATE `creature_text` SET text = 'Aquí me hallo, como el lobo ante los corderos... y no me temen.' WHERE text LIKE '%Now%I%stand,%the%lion%before%the%lambs...%and%they%do%not%fear.%';
+
+UPDATE `creature_text` SET text = 'No sienten miedo...' WHERE text LIKE '%They%cannot%fear.%';
+
+UPDATE `creature_text` SET text = 'Temblad ante Taldaram, mortales, ¡el poder del orbe fluye en mí!' WHERE text LIKE '%Tremble%before%Taldaram,%mortals,%for%the%power%of%the%orb%flows%through%me!%';
+
+UPDATE `creature_text` SET text = '¿Qué? ¿Esta fuerza...? ¡Ya no hay dolor! Tú... ¡debes unirte a mi en el abrazo eterno de la muerte!' WHERE text LIKE '%¿Qué?%Esta%fuerza...?%¡Ya%no%hay%dolor!%Tú...%¡debes%unirte%a%mi%en%el%abrazo%eterno%de%la%muerte!%';
+
+UPDATE `creature_text` SET text = 'Lo siento...',  = 'Lo siento...' WHERE text LIKE '%I\'m%sorry...%';
+
+UPDATE `creature_text` SET text = 'No me encuentro bien...' WHERE text LIKE '%No%me%encuentro%bien...%Gyah!%Uhhh...%';
+
+UPDATE `creature_text` SET text = '¿Quién... anda ahí?' WHERE text LIKE '%Who...%goes%there...%';
+
+UPDATE `creature_text` SET text = 'Estoy despierto...' WHERE text LIKE '%I...%awaken!%';
+
+UPDATE `creature_text` SET text = 'El sagrario del maestro ha sido perturbado.' WHERE text LIKE '%The%master\'s%sanctum%has%been%disturbed!%';
+
+UPDATE `creature_text` SET text = '¡Kor\'kron, preparad el Martillo de Orgrim para su último viaje! Campeones, nuestra nave atracará en la parte superior de la Ciudadela. ¡Nos veremos allí!' WHERE text LIKE '%Kor\'kron,%prepare%Orgrim\'s%Hammer%for%its%final%voyage!%Champions,%our%gunship%will%find%a%point%to%dock%on%the%upper%reaches%of%the%citadel.%Meet%us%there!%';
+
+UPDATE `creature_text` SET text = '¡Libre al fin! Se acabó, hijo mío. Es la hora del juicio final.' WHERE text LIKE '%Free%at%last!%It%is%over,%my%son.%This%is%the%moment%of%reckoning.%';
+
+UPDATE `creature_text` SET text = '¡ALZAOS, CAMPEONES DE LA LUZ!' WHERE text LIKE '%Rise%up,%champions%of%the%Light!%';
+
+UPDATE `creature_text` SET text = '¡Ayudadme a destruir estas almas torturadas! ¡Juntos debilitaremos el poder de la Agonía de Escarcha, y al Rey Exánime desde dentro!' WHERE text LIKE '%Aid%me%in%destroying%these%tortured%souls!%Together%we%will%loosen%Frostmourne\'s%hold%and%weaken%the%Lich%King%from%within!%';
+
+UPDATE `creature_text` SET text = 'Primero, escapad del abrazo de la Agonía de Escarcha, o estaréis condenados como yo, atrapado en esta maldita espada toda la eternidad.' WHERE text LIKE '%First,%you%must%escape%Frostmourne\'s%hold,%or%be%damned%as%I%am;%trapped%within%this%cursed%blade%for%all%eternity.%';
+
+UPDATE `creature_text` SET text = '¿Habéis venido para ajusticiar a Arthas? ¿Para ver la destrucción del Rey Exánime?' WHERE text LIKE '%You%have%come%to%bring%Arthas%to%justice?%To%see%the%Lich%King%destroyed?%';
+
+UPDATE `creature_text` SET text = '¡Agonía de Escarcha! ¡Obedéceme!' WHERE text LIKE '%Argh...%Frostmourne,%obey%me!%';
+
+UPDATE `creature_text` SET text = '¡La Agonía de Escarcha se alimenta del alma de vuestro aliado!' WHERE text LIKE '%Frostmourne%feeds%on%the%soul%of%your%fallen%ally!%';
+
+UPDATE `creature_text` SET text = ' Esta es nuestra batalla final. El paso del tiempo se hará eco de lo que aquí ocurra. Sin importar el resultado, sabrán que luchamos con honor. ¡Que luchamos por la libertad y la seguridad de nuestro pueblo!' WHERE text LIKE '%This%is%our%final%stand.%What%happens%here%will%echo%through%the%ages.%Regardless%of%outcome,%they%will%know%that%we%fought%with%honor.%That%we%fought%for%the%freedom%and%safety%of%our%people!%';
+
+UPDATE `creature_text` SET text = 'Recordad, héroes, el miedo es el mayor enemigo en estas cámaras infectas. Fortaleced vuestro corazón y vuestra alma brillará más que mil soles. El enemigo vacilará al veros. ¡Caerán en cuanto la luz de la rectitud los envuelva!' WHERE text LIKE '%Remember,%heroes,%fear%is%your%greatest%enemy%in%these%befouled%halls.%Steel%your%heart%and%your%soul%will%shine%brighter%than%a%thousand%suns.%The%enemy%will%falter%at%the%sight%of%you.%They%will%fall%as%the%light%of%righteousness%envelops%them!%';
+
+UPDATE `creature_text` SET text = '¡Marchemos hacia la Ciudadela de Corona de Hielo ahora!' WHERE text LIKE '%Our%march%upon%Icecrown%Citadel%begins%now!%';
+
+UPDATE `creature_text` SET text = 'Ahora estás sobre la tierra sagrada de la Plaga. La Luz no te protegerá aquí, paladín. Nada te protegerá...' WHERE text LIKE '%You%now%stand%upon%the%hallowed%ground%of%the%Scourge.%The%Light%won\'t%protect%you%here,%paladin.%Nothing%will%protect%you...%';
+
+UPDATE `creature_text` SET text = '¡ARTHAS! ¡Juré que te vería muerto y a la Plaga desmantelada! ¡Voy a acabar lo que comencé en la Esperanza de la Luz!' WHERE text LIKE '%ARTHAS!%I%swore%that%I%would%see%you%dead%and%the%Scourge%dismantled!%I\'m%going%to%finish%what%I%started%at%Light\'s%Hope!%';
+
+UPDATE `creature_text` SET text = 'Podrías haber sido el mejor de mis campeones, Vadín: una fuerza oscura que asolaría el mundo y lo llevaría a una nueva era de conflictos.' WHERE text LIKE '%You%could%have%been%my%greatest%champion,%Fordring:%A%force%of%darkness%that%would%wash%over%this%world%and%deliver%it%into%a%new%age%of%strife.%';
+
+UPDATE `creature_text` SET text = 'Pero ese honor ya no te pertenece. Pronto, tendré un nuevo campeón.' WHERE text LIKE '%But%that%honor%is%no%longer%yours.%Soon,%I%will%have%a%new%champion.%';
+
+UPDATE `creature_text` SET text = 'El adiestramiento ha sido agotador. Las atrocidades que he cometido sobre su alma... ha resistido tanto tiempo, pero pronto se inclinará ante su Rey.' WHERE text LIKE '%The%breaking%of%this%one%has%been%taxing.%The%atrocities%I%have%committed%upon%his%soul.%He%has%resisted%for%so%long,%but%he%will%bow%down%before%his%king%soon.%';
+
+UPDATE `creature_text` SET text = '¡NUNCA! Nunca... te... serviré...' WHERE text LIKE '%NEVER!%I...%I%will%never...%serve...%you...%';
+
+UPDATE `creature_text` SET text = 'Al final, todos me serviréis.' WHERE text LIKE '%In%the%end,%you%will%all%serve%me.%';
+
+UPDATE `creature_text` SET text = '¿El paladín aún vive? ¿Cómo es posible, Alto señor? ¿Cómo pudo sobrevivir?' WHERE text LIKE '%The%paladin%still%lives?%Is%it%possible,%High%Lord?%Could%he%have%survived?%';
+
+UPDATE `creature_text` SET text = 'El poder de la Luz no tiene límites, Colmillosauro. Su alma está bajo una gran tensión, pero vive, por ahora.' WHERE text LIKE '%The%power%of%the%light%knows%no%bounds,%Saurfang.%His%soul%is%under%great%strain,%but%he%lives...%for%now.%';
+
+UPDATE `creature_text` SET text = '¡Debemos salvarle! Si rescatamos a Bolvar Fordragón, quizá calmemos las tensiones entre la Alianza y la Horda.' WHERE text LIKE '%Then%we%must%save%him.%If%we%rescue%Bolvar%Fordragon%we%may%quell%the%unrest%between%the%Alliance%and%the%Horde.%';
+
+UPDATE `creature_text` SET text = 'Nuestra misión está clara: el Rey Exánime responderá por sus crímenes y salvaremos al alto señor Bolvar Fordragón.' WHERE text LIKE '%Our%mission%is%now%clear.%The%Lich%King%will%answer%for%his%crimes,%and%we%will%save%Highlord%Bolvar%Fordragon.%';
